3ce45b96ccedf232fb362c436a0d8f3c9186d7cf
Aidan <quantumferret@users.noreply.github.com>
2021-06-15 18:16:11 +0200

    Add avr-atmega328p to the exclusion list for targets that have atomics.
    It's worth noting that the avr-atmega328p only has atomic U8s, not larger, so this may be a futile change...

--- build.rs
+++ build.rs
@@ -32,7 +32,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
 
     match &target[..] {
-        "msp430-none-elf" | "riscv32i-unknown-none-elf" | "riscv32imc-unknown-none-elf" => {}
+        "msp430-none-elf" | "riscv32i-unknown-none-elf" | "riscv32imc-unknown-none-elf" | "avr-atmega328p" => {}
 
         _ => {
             println!("cargo:rustc-cfg=has_atomics");
